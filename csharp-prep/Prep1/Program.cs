ee0971e32d183a437fb6eeb8873a1ee7061d8722
tsanders107 <145694545+tsanders107@users.noreply.github.com>
2026-09-18 16:03:04 -0600

    prep1and2

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Console.WriteLine("Hello Prep1 World!");
+        
+        Console.Write("What is your first Name? ");
+        string first = Console.ReadLine();
+
+        Console.Write("What is your last Name? ");
+        string last = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last}, {first} {last}.");
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,54 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string gradePercentage = Console.ReadLine();
+        int number = int.Parse(gradePercentage);
+
+        string lettergrade = "";
+
+        if (number >= 96)
+            {
+                lettergrade = "A+";
+            }
+
+            else if (number == 95)
+                {
+                    lettergrade = "A";
+                }
+
+            else if (number >= 94)
+                {
+                    lettergrade = "A-";
+                }
+
+        else if (number >= 80)
+            {
+                lettergrade = "B";
+            }
+
+        else if (number >= 70)
+            {
+                lettergrade = "C";
+            }
+
+        else if (number >= 60)
+            {
+                lettergrade = "D";
+            }
+
+        else if (number <= 59)
+            {
+                lettergrade = "F";
+            }
+
+        Console.WriteLine($"Your letter grade is {lettergrade}");
+
+        if (number >= 70)
+            {
+                Console.WriteLine("Good job you passed :)");
+            }
+        else Console.WriteLine("Keep trying!");
+
     }
 }
